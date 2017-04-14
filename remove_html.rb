3c2f3969dbfa60322b874396bcef13b56ae4b815
chris smith <cleesmith2006@gmail.com>
2017-04-14 18:34:36 -0400

    update

---
+++ remove_html.rb
@@ -0,0 +1,21 @@
+require 'rubygems'
+require 'nokogiri'
+
+TIME_REGEXP = /\d{1,}:\d{2}:\d{2}/
+
+cc = File.open("ccSubs_com_waiting-for-magic-alan-watts_en.sbv") do |f|
+	Nokogiri::HTML(f)
+end
+cc = cc.text
+# cc = cc.text.gsub /^$\n/, '' # delete all blank lines
+
+open('cc.txt', 'w') do |f|
+	cc.each_line do |line|
+		line.strip!
+		next if line.length <= 0
+		# ignore timestamp lines, which fails if spoken
+		# words include something like a timestamp:
+		next if line.match(TIME_REGEXP)
+	  f.puts line
+	end
+end

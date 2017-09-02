fbe72bc42262fe8d3910175277bb526ad07eea20
Chris Smith <cleesmith2006@gmail.com>
2017-09-02 19:36:03 -0400

    update

--- remove_html.rb
+++ remove_html.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 
 TIME_REGEXP = /\d{1,}:\d{2}:\d{2}/
 
-cc = File.open("16-witchy-questions.sbv") do |f|
+cc = File.open("ccSubs_com_discussing-black-magick-dark-witchcraft-part-2_en.sbv") do |f|
 	Nokogiri::HTML(f)
 end
 cc = cc.text

8dcdff674a86bc13170444ea2dc3dc83dde0391d
chris <cleesmith2006@gmail.com>
2022-08-10 18:22:53 -0400

    clean up, remove Pixie

--- js/deal.js
+++ js/deal.js
@@ -134,7 +134,8 @@ var dealCard = function(i) {
 		var orient = 0; //randomOrient(); <-- allows reversals
 		$(img).addClass(cardJustDealt);
 		// addClass for card's position (1-7)
-		img.src = ("images/deck/" + cards[i] + ".jpg");
+		//img.src = ("images/deck/" + cards[i] + ".jpg");
+    img.src = ("images/taropian_songs_deck/0_Fool_tiff.jpg");
 		img.alt = cards[i];
 
 		if (orient === 1) {
